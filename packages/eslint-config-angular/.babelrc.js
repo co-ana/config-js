a2543294912af45109282d542fca4e02c1f22e39
Ana Có <ana.cbe14@gmail.com>
2023-12-14 19:40:17 -0300

    chore: update angular configs

---
+++ packages/eslint-config-angular/.babelrc.js
@@ -0,0 +1,10 @@
+require("@babel/parser").parse("code", {
+  // parse in strict mode and allow module declarations
+  sourceType: "module",
+
+  plugins: [
+    // enable jsx and flow syntax
+    "jsx",
+    "flow",
+  ],
+});
--- packages/eslint-config-angular/.eslintrc.js
+++ packages/eslint-config-angular/.eslintrc.js
@@ -15,5 +15,52 @@ module.exports = {
     "@angular-eslint/no-outputs-metadata-property": "error",
     "@angular-eslint/use-pipe-transform-interface": "error",
     "@angular-eslint/use-lifecycle-interface": "warn"
-  }
+  },
+  "overrides": [
+    /**
+     * -----------------------------------------------------
+     * TYPESCRIPT FILES (COMPONENTS, SERVICES ETC) (.ts)
+     * -----------------------------------------------------
+     */
+    {
+      "files": ["*.ts"],
+      "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@angular-eslint/recommended",
+        // This is required if you use inline templates in Components
+        "plugin:@angular-eslint/template/process-inline-templates"
+      ],
+
+      rules: {
+        "@angular-eslint/directive-selector": [
+          "error",
+          { "type": "attribute", "prefix": "app", "style": "camelCase" }
+        ],
+        "@angular-eslint/component-selector": [
+          "error",
+          { "type": "element", "prefix": "app", "style": "kebab-case" }
+        ]
+      }
+    },
+
+    /**
+     * -----------------------------------------------------
+     * COMPONENT TEMPLATES
+     * -----------------------------------------------------
+     */
+    {
+      "files": ["*.html"],
+      "extends": [
+        "plugin:@angular-eslint/template/recommended",
+        "plugin:@angular-eslint/template/accessibility"
+      ],
+      "rules": {
+        /**
+         * Any template/HTML related rules you wish to use/reconfigure over and above the
+         * recommended set provided by the @angular-eslint project would go here.
+         */
+      }
+    }
+  ]
 };
